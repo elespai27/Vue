6b9d68f470181b98f43e6ade42d2a3f3550cca0a
elespai27@gmail.com <elespai27@gmail.com>
2025-04-02 11:07:45 +0200

    project create

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+
+<!-- esto es lógica JS -->
+<script setup>
+
+import { ref } from 'vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+// esto es un componente
+
+let text = "Sou un texo que viene de una variable";
+const counter = ref(0);
+
+setInterval(() => {
+counter.value = counter.value + 1;
+console.log(counter)
+}, 1000)
+
+</script>
+
+<!-- esto es HTML -->
+<template>
+  <h2 class="title">Bienvenidos a mi web</h2>
+  <HeaderComponent></HeaderComponent>
+
+  <h2>{{ counter }}</h2>
+
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    Nihil nemo cum dicta, doloribus deserunt repudiandae dolorem
+    eveniet dolores quibusdam amet laborum. Numquam dicta mollitia ex
+    nisi beatae! Nisi, velit incidunt!
+  </p>
+
+  <p>{{ text }}</p>
+  <!-- notacion de bigote para que salga el contenido de la var en el contenido web -->
+
+</template>
+
+<!-- esto es CSS -->
+<style>
+.title {
+    background-color: blueviolet;
+}
+</style>
+
+<!-- Componentes independientes para reutilizarlos en el framework
+ no puedo poner los mismos nombres que tenemos en el archivo
+ y comienzan en mayúsculas en los archivos-->
+
+ <!-- variables reactivas es otro concepto -->
+
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Hola a todos</h1>
+  <nav>
+    <ul class="nav-list">
+      <li><a href="#" class="link">HOME</a></li>
+      <li><a href="#" class="link">HOME</a></li>
+      <li><a href="#" class="link">HOME</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.header {
+  background-color: coral;
+}
+
+</style>
