34129c5dc860328105e434ac8f74814e2282fdf2
elespai27 <elespai27@gmail.com>
2025-04-03 22:50:12 +0200

    variables&events

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,75 @@
-
 <!-- esto es lógica JS -->
 <script setup>
-
-import { ref } from 'vue';
-import HeaderComponent from './components/HeaderComponent.vue';
+import { ref } from 'vue' //esto es una función de Vue
+import HeaderComponent from './components/HeaderComponent.vue'
 // esto es un componente
 
-let text = "Sou un texo que viene de una variable";
-const counter = ref(0);
+let text = 'Soy un texo que viene de una variable'
+const counter = ref(0)
+const userName = ref("")
+const clickX= ref(0);
+const clickY= ref(0);
+
+
+//const userName = ref("") //valor inicial dentro de paréntisis
+const imgInfo = {
+  url: 'https://plus.unsplash.com/premium_photo-1709579654090-3f3ca8f8416b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJhbGV6YSUyMHBhaXNhamV8ZW58MHx8MHx8fDA%3D',
+  alt: 'Imagen de un paiosaje', //texto alternativo para accesibilidad
+  width: 300,
+}
 
 setInterval(() => {
-counter.value = counter.value + 1;
-console.log(counter)
+  counter.value = counter.value + 1 //para usar esta var reactiva tenemos que hacer .value
+  console.log(counter)
 }, 1000)
+
+function showAlert() {
+  alert('Hola a todos')
+}
+function chandeUserName() {
+  userName.value = '_Vero';
+}
+
+function handleClick(event) {
+  console.log(event);
+  clickX.value = event.screenX;
+  clickY.value = event.screenY;//el scrreenX y el screenY son las coordenadas del mouse
+}
+// el event es un objeto que contiene información del evento
 
 </script>
 
 <!-- esto es HTML -->
 <template>
-  <h2 class="title">Bienvenidos a mi web</h2>
   <HeaderComponent></HeaderComponent>
 
+  <img :src="imgInfo.url" :alt="imgInfo.alt" :width="imgInfo.width" />
+  <!-- v-bind -->
+
+  <h2>{{ userName }}</h2>
+  <input type="text" placeholder="nombre" v-model="userName"> <!-- v-model -->
+  <br>
+  <button @click="chandeUserName">change userName</button>
   <h2>{{ counter }}</h2>
 
-  <p>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-    Nihil nemo cum dicta, doloribus deserunt repudiandae dolorem
-    eveniet dolores quibusdam amet laborum. Numquam dicta mollitia ex
+  <h2 class="title" @click="showAlert">Bienvenidos a mi web</h2>
+  <!-- el arroba + nombre del evento + función (Esto es un evento) -->
+
+  <p @click="handleClick">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil nemo cum dicta, doloribus
+    deserunt repudiandae dolorem eveniet dolores quibusdam amet laborum. Numquam dicta mollitia ex
     nisi beatae! Nisi, velit incidunt!
   </p>
+  <h3>{{ clickX }} - Y: {{ clickY }}</h3>
 
   <p>{{ text }}</p>
   <!-- notacion de bigote para que salga el contenido de la var en el contenido web -->
-
 </template>
 
 <!-- esto es CSS -->
 <style>
 .title {
-    background-color: blueviolet;
+  background-color: blueviolet;
 }
 </style>
 
@@ -46,5 +77,4 @@ console.log(counter)
  no puedo poner los mismos nombres que tenemos en el archivo
  y comienzan en mayúsculas en los archivos-->
 
- <!-- variables reactivas es otro concepto -->
-
+<!-- variables reactivas es otro concepto -->
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,9 +6,9 @@
   <h1>Hola a todos</h1>
   <nav>
     <ul class="nav-list">
-      <li><a href="#" class="link">HOME</a></li>
-      <li><a href="#" class="link">HOME</a></li>
-      <li><a href="#" class="link">HOME</a></li>
+      <li><a href="#" class="link">Home</a></li>
+      <li><a href="#" class="link">Blog</a></li>
+      <li><a href="#" class="link">Contact Us</a></li>
     </ul>
   </nav>
 </template>
@@ -16,6 +16,27 @@
 <style>
 .header {
   background-color: coral;
+  color: rgb(4, 57, 31);
+  padding: 20px;
+  .header-title {
+    margin: 0;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+
+  .nav-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+
+    .link {
+      color: rgb(17, 209, 168);
+      text-decoration: none;
+    }
+  }
+
 }
 
 </style>
